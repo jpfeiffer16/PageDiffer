e23b9cfdd695350f06d712c01c19431edd81e0ee
jpfeiffer16 <jpfeiffer@bluemodus.com>
2016-06-23 06:45:26 -0600

    modularizing

--- app.js
+++ app.js
@@ -8,45 +8,58 @@ program
   .version(require('./package.json').version)
   .option('-t, --target [string]', 'Target webpage')
   .option('-s, --source [string]', 'Source webpage')
+  .option('-p, --pipe [boolean]', 'Output Compare image data to stdout')
   .parse(process.argv);
 
-var website1 = program.source,
-    website2 = program.target,
-    website1Image = __dirname + '/' + website1 + '.png',
-    website2Image = __dirname + '/' + website2 + '.png';
+//TODO: Do some parameter checking here
+getDiff(program.source, program.target, __dirname + '/', function(info) {
+  if (program.pipe)
+    console.log(info.data);
+  else
+    console.log('Similarity: ', info.similarity, '%');
+});
 
-webshot(website1, website1Image, function(err) {
-  if (err) {
-    console.warn(err);
-    return process.exit(1);
-  }
-  webshot(website2, website2Image, function(err) {
+//var website1 = program.source,
+//    website2 = program.target,
+//    website1Image = __dirname + '/' + website1 + '.png',
+//    website2Image = __dirname + '/' + website2 + '.png';
+
+function getDiff(sourceUrl, targetUrl, dir, callback) {
+  var website1Image = dir + sourceUrl + '.png';
+  var website2Image = dir + targetUrl + '.png';
+  webshot(sourceUrl, website1Image, function(err) {
     if (err) {
-      console.warn(err);
+      console.error(err);
       return process.exit(1);
     }
-    resemble(website1Image).compareTo(website2Image)
-      .onComplete(function(data) {
-      console.dir(data); 
-      var png = data.getDiffImage(),
-          pngBuffer = new Buffer([]),
-          pngStream = png.pack();
-      pngStream.on('data', function(data) {
-        pngBuffer = Buffer.concat([pngBuffer, data]);
-      });
-      pngStream.on('end', function() {
-        var base64 = pngBuffer.toString('base64');
-        console.log('Done getting base64 data');
-        fs.writeFile(__dirname + '/compare.png',
-            base64, 'base64', function(err) {
-          if (err) {
-            console.warn(err);
-            process.exit(1);
-          }
-          console.log('Compare file has been written');
+    webshot(targetUrl, website2Image, function(err) {
+      if (err) {
+        console.error(err);
+        return process.exit(1);
+      }
+      resemble(website1Image).compareTo(website2Image)
+        .onComplete(function(data) {
+        var png = data.getDiffImage(),
+            pngBuffer = new Buffer([]),
+            pngStream = png.pack();
+        pngStream.on('data', function(data) {
+          pngBuffer = Buffer.concat([pngBuffer, data]);
+        });
+        pngStream.on('end', function() {
+          var base64 = pngBuffer.toString('base64');
+          fs.writeFile(dir + 'compare.png',
+              base64, 'base64', function(err) {
+            if (err) {
+              console.error(err);
+              process.exit(1);
+            }
+            if (typeof callback == 'function') callback( {
+              similarity: 100 - data.misMatchPercentage,
+              data: base64
+            });
+          });
         });
       });
     });
   });
-});
-
+}
