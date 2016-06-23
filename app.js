dbaaa9992af3a4f00e931a03a64a710bf872725b
jpfeiffer16 <jpfeiffer@bluemodus.com>
2016-06-22 22:47:24 -0600

    Changes to add commander and global install

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-var webshot = require('webshot');
-var resemble = require('node-resemble-js');
-var fs = require('fs');
+#!/usr/bin/env node
+var webshot = require('webshot'),
+    resemble = require('node-resemble-js'),
+    fs = require('fs'),
+    program = require('commander');
 
-var website1 = 'google.com';
-var website2 = 'google.co.uk';
-var website1Image = __dirname + '/' + website1 + '.png';
-var website2Image = __dirname + '/' + website2 + '.png';
-console.log(website1Image, website2Image);
+program
+  .version(require('./package.json').version)
+  .option('-t, --target [string]', 'Target webpage')
+  .option('-s, --source [string]', 'Source webpage')
+  .parse(process.argv);
+
+var website1 = program.source,
+    website2 = program.target,
+    website1Image = __dirname + '/' + website1 + '.png',
+    website2Image = __dirname + '/' + website2 + '.png';
 
 webshot(website1, website1Image, function(err) {
   if (err) {
@@ -18,7 +25,8 @@ webshot(website1, website1Image, function(err) {
       console.warn(err);
       return process.exit(1);
     }
-    resemble(website1Image).compareTo(website2Image).onComplete(function(data) {
+    resemble(website1Image).compareTo(website2Image)
+      .onComplete(function(data) {
       console.dir(data); 
       var png = data.getDiffImage(),
           pngBuffer = new Buffer([]),
@@ -29,7 +37,8 @@ webshot(website1, website1Image, function(err) {
       pngStream.on('end', function() {
         var base64 = pngBuffer.toString('base64');
         console.log('Done getting base64 data');
-        fs.writeFile(__dirname + '/compare.png', base64, 'base64', function(err) {
+        fs.writeFile(__dirname + '/compare.png',
+            base64, 'base64', function(err) {
           if (err) {
             console.warn(err);
             process.exit(1);
